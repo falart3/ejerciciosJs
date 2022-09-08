c57eae1434e8bd66f9eee962f063b80e0575c3a0
falart3 <112985338+falart3@users.noreply.github.com>
2022-09-09 00:32:09 +0200

    Add files via upload

---
+++ primero.js
@@ -0,0 +1,14 @@
+const message = 'Hello world' // Try edit me
+var nombre= 'Francisco'
+var texto= 'Mi nombre es '
+var decir= ', y estoy aprendiendo js.'
+// Update header text
+//document.querySelector('#header').innerHTML = message
+document.write(message+"! <br>"+texto+nombre+decir)
+// var sp="&nbsp;";
+// var esp="";
+// var n=0;
+// while (n++ <10) esp=esp+sp;
+// document.body.innerHTML="hola"+esp+"chao";
+// Log to console
+console.log(message)
